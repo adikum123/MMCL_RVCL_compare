2a135d1f0b1da930e20c0534ce83395478791e8c
Admir <admir@qyobo.com>
2024-12-27 00:59:44 +0100

    Add python files

---
+++ mmcl/mmcl.py
@@ -0,0 +1,100 @@
+import json
+from typing import Union
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from tqdm import tqdm
+
+import mmcl.utils as utils
+import rocl.data_loader as data_loader
+from mmcl.svm_losses import MMCL_inv, MMCL_pgd
+
+
+class MMCL_Encoder(nn.Module):
+
+    def __init__(self, hparams, device):
+        super(MMCL_Encoder, self).__init__()
+        self.hparams = hparams
+        if self.hparams.criterion_to_use == 'mmcl_inv':
+            self.crit = MMCL_inv(
+                sigma=self.hparams.gamma,
+                batch_size=self.hparams.batch_size,
+                anchor_count=2,
+                C=self.hparams.C
+            )
+        elif self.hparams.criterion_to_use == 'mmcl_pgd':
+            self.crit = MMCL_pgd(
+                sigma=self.hparams.gamma,
+                batch_size=self.hparams.batch_size,
+                anchor_count=2,
+                C=self.hparams.C,
+                solver_type=self.hparams.solver_type,
+                use_norm=self.hparams.use_norm
+            )
+        self.device = device
+        self.model = utils.load_model_contrastive(args=self.hparams, weights_loaded=False)
+        self.trainloader, self.traindst, self.testloader, self.testdst = data_loader.get_dataset(self.hparams)
+        self.optimizer = optim.SGD(
+            self.model.parameters(),
+            lr=self.hparams.lr,
+            weight_decay=1e-6
+        )
+        self.scheduler = optim.lr_scheduler.StepLR(self.optimizer, step_size=self.hparams.step_size, gamma=self.hparams.gamma)
+
+    def step(self, batch):
+        original_batch, transformed_batch, transformed_batch_2, target = batch
+        z = self.model(batch)
+        loss, kxz, kyz, sparsity, num_zero, acc = self.criterion(z)
+        return {
+            "loss": loss,
+            "contrast_acc": acc,
+            "kxz": kxz,
+            "kyz": kyz,
+            "sparsity": sparsity,
+            "num_zero": num_zero,
+        }
+
+    def forward(self, *args, **kwargs):
+        return self.model(*args, **kwargs)
+
+    def train_step(self, batch, it=None):
+        logs = self.step(batch)
+
+        if self.hparams.dist == "ddp":
+            self.trainsampler.set_epoch(it)
+        if it is not None:
+            logs["epoch"] = it / len(self.batch_trainsampler)
+        return logs
+
+    def train_epoch(self, epoch):
+        self.model.train()
+        total_loss, total_num, train_bar = 0.0, 0, tqdm(self.trainloader)
+        kxz_losses,kyz_losses = 0.0, 0.0
+        for iii, (pos_1, pos_2, target, index) in enumerate(train_bar):
+            pos_1, pos_2 = pos_1.to(self.device,non_blocking=True), pos_2.to(self.device,non_blocking=True)
+            feature_1 = self.model(pos_1)
+            feature_2 = self.model(pos_2)
+
+            features = torch.cat([feature_1.unsqueeze(1), feature_2.unsqueeze(1)], dim=1)
+
+            loss = self.crit(features)
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+
+            total_num += self.hparams.batch_size
+            total_loss += loss.item() * self.hparams.batch_size
+            train_bar.set_description('Train Epoch: [{}/{}] Loss: {:.4e}'.format(epoch, self.hparams.num_iters, total_loss / total_num))
+        self.scheduler.step()
+        metrics = {
+            'total_loss':total_loss / total_num,
+            'epoch': epoch
+        }
+        return metrics
+
+    def train(self):
+        for epoch in range(self.hparams.num_iters):
+            metrics = self.train_epoch(epoch=epoch)
+            metrics['epoch'] = epoch
+            print(f'Epoch: {epoch+1}, metrics: {json.dumps(metrics, indent=4)}')
---
+++ mmcl/utils.py
@@ -0,0 +1,160 @@
+import torch
+import torch.nn as nn
+
+
+class Flatten(nn.Module):
+    def forward(self, x):
+        return x.view(x.size(0), -1)
+
+
+def cifar_model():
+    # cifar base
+    model = nn.Sequential(
+        nn.Conv2d(3, 8, 4, stride=2, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(8, 16, 4, stride=2, padding=1),
+        nn.ReLU(),
+        Flatten(),
+        nn.Linear(1024, 100),
+        nn.ReLU(),
+        nn.Linear(100, 10)
+    )
+    return model
+
+def mnist_model_base():
+    # mnist base
+    model = nn.Sequential(
+        nn.Conv2d(1, 8, 4, stride=2, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(8, 16, 4, stride=2, padding=1),
+        nn.ReLU(),
+        Flatten(),
+        nn.Linear(784, 100),
+        nn.ReLU(),
+        nn.Linear(100, 10)
+    )
+    return model
+
+def cifar_model_deep():
+    # cifar deep
+    model = nn.Sequential(
+        nn.Conv2d(3, 8, 4, stride=2, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(8, 8, 3, stride=1, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(8, 8, 3, stride=1, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(8, 8, 4, stride=2, padding=1),
+        nn.ReLU(),
+        Flatten(),
+        nn.Linear(8*8*8, 100),
+        nn.ReLU(),
+        nn.Linear(100, 10)
+    )
+    return model
+
+def mnist_model_deep():
+    # mnist deep
+    model = nn.Sequential(
+        nn.Conv2d(1, 8, 4, stride=2, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(8, 8, 3, stride=1, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(8, 8, 3, stride=1, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(8, 8, 4, stride=2, padding=1),
+        nn.ReLU(),
+        Flatten(),
+        nn.Linear(392, 100),
+        nn.ReLU(),
+        nn.Linear(100, 10)
+    )
+    return model
+
+def cifar_model_wide():
+    # cifar wide
+    model = nn.Sequential(
+        nn.Conv2d(3, 16, 4, stride=2, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(16, 32, 4, stride=2, padding=1),
+        nn.ReLU(),
+        Flatten(),
+        nn.Linear(32*8*8,100),
+        nn.ReLU(),
+        nn.Linear(100, 10)
+    )
+    return model
+
+
+def cnn_4layer():
+    # cifar_cnn_a
+    return cifar_model_wide()
+
+
+def cnn_4layer_b():
+    # cifar_cnn_b
+    return nn.Sequential(
+        nn.ZeroPad2d((1,2,1,2)),
+        nn.Conv2d(3, 32, (5,5), stride=2, padding=0),
+        nn.ReLU(),
+        nn.Conv2d(32, 128, (4,4), stride=2, padding=1),
+        nn.ReLU(),
+        Flatten(),
+        nn.Linear(8192, 100),
+        nn.ReLU(),
+        nn.Linear(100, 10),
+    )
+
+def mnist_cnn_4layer_b():
+    # mnist_cnn_b
+    return nn.Sequential(
+        nn.ZeroPad2d((1,2,1,2)),
+        nn.Conv2d(1, 32, (5,5), stride=2, padding=0),
+        nn.ReLU(),
+        nn.Conv2d(32, 128, (4,4), stride=2, padding=1),
+        nn.ReLU(),
+        Flatten(),
+        nn.Linear(6272, 100),
+        nn.ReLU(),
+        nn.Linear(100, 10),
+    )
+
+def mnist_cnn_4layer():
+    # mnist_cnn_a
+    return nn.Sequential(
+        nn.Conv2d(1, 16, (4,4), stride=2, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(16, 32, (4,4), stride=2, padding=1),
+        nn.ReLU(),
+        Flatten(),
+        nn.Linear(1568, 100),
+        nn.ReLU(),
+        nn.Linear(100, 10),
+    )
+
+def cut_model(model, contrastive=True, linear=False):
+    if contrastive:
+        return nn.Sequential(*list(model.children())[:-2])
+    if linear:
+        return nn.Sequential(*list(model.children())[-1])
+    return model
+
+def load_model_contrastive(args, weights_loaded=True, contrastive=True, linear=False):
+    """
+    Load the model architectures and weights
+    """
+    model_ori = eval(args.model)()
+    print(f'Type of model after eval: {type(model_ori)}')
+    if not weights_loaded:
+        return cut_model(model_ori, contrastive, linear)
+    print('loading weight...')
+    map_location = None
+    if args.device == 'cpu':
+        map_location = torch.device('cpu')
+
+    if 'cnn_4layer' not in args.model:
+        model_ori.load_state_dict(torch.load(model_path(args), map_location)['state_dict'][0])
+    else:
+        model_ori.load_state_dict(torch.load(model_path(args), map_location))
+
+    return cut_model(model_ori, contrastive, linear)
