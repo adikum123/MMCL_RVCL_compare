8d6423327dedb9f8227163db7dc8c2f24e7308f3
Admir <admir@qyobo.com>
2025-01-06 17:47:11 +0100

    Add Dockerfile

--- train_test_encoder.cmd
+++ train_test_encoder.cmd
@@ -5,19 +5,23 @@
 #SBATCH -o outs/100k.out
 #SBATCH -e errs/100k.err
 
+enroot remove $CONTAINER_NAME
+
 CONTAINER_IMAGE="nvidia+tensorflow+20.12-tf1-py3.sqsh"
 CONTAINER_NAME="mmcl_rvcl"
 PYTHON_SCRIPT="mmcl/train_test_encoder.py"
 REQUIREMENTS_FILE="requirements.txt"
 
+docker build -t custom-tensorflow:py3.7 .
+docker save custom-tensorflow:py3.7 | enroot import - $CONTAINER_IMAGE
 enroot create --name $CONTAINER_NAME $CONTAINER_IMAGE
-enroot start --m MMCL_RVCL_compare/ $CONTAINER_NAME << EOF
-    cd ..
-    cd MMCL_RVCL_compare/
-    pip install --upgrade pip
-    pip install -r $REQUIREMENTS_FILE
-    export PYTHONPATH=$(pwd):$PYTHONPATH
-EOF
+enroot start --m MMCL_RVCL_compare/ $CONTAINER_NAME
+cd ..
+cd MMCL_RVCL_compare/
+pip install --upgrade pip
+pip install -r $REQUIREMENTS_FILE
+export PYTHONPATH=$(pwd):$PYTHONPATH
+
 
 python $PYTHON_SCRIPT --kernel_type rbf
 
