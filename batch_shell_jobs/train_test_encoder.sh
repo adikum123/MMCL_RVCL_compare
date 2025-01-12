bb6de2f4f81ae5ce8e9cf772cd625c3cced558c6
Admir <admir@qyobo.com>
2025-01-12 17:28:58 +0100

    Update script

--- batch_shell_jobs/train_test_encoder.sh
+++ batch_shell_jobs/train_test_encoder.sh
@@ -11,37 +11,43 @@ PYTHON_VERSION="3.7"
 TORCH_URL="https://download.pytorch.org/whl/torch-1.6.0%2Bcu101-cp37-cp37m-linux_x86_64.whl"
 
 echo "Creating and starting the container..."
-enroot create --name $CONTAINER_NAME $CONTAINER_IMAGE || echo "Container already exists"
-enroot start --root --mount $(pwd):/workspace $CONTAINER_NAME
-
-[ -d ~/.pyenv ] && rm -rf ~/.pyenv
-echo "Installing pyenv and pyenv-virtualenv..."
-apt update
-apt install git -y
-apt install curl -y
-apt install -y build-essential gcc make libffi-dev zlib1g-dev libssl-dev libreadline-dev libbz2-dev libsqlite3-dev
-curl https://pyenv.run | bash
-export PATH="$HOME/.pyenv/bin:$PATH"
-eval "$(pyenv init --path)"
-eval "$(pyenv virtualenv-init -)"
-
-if ! pyenv versions | grep -q $PYTHON_VERSION; then
-    echo "Installing Python $PYTHON_VERSION with pyenv..."
-    pyenv install $PYTHON_VERSION
+# Check if the container exists
+if ! enroot list | grep -q "^$CONTAINER_NAME$"; then
+    echo "Container $CONTAINER_NAME does not exist. Creating it..."
+    enroot create --name $CONTAINER_NAME $CONTAINER_IMAGE
+else
+    echo "Container $CONTAINER_NAME already exists. Skipping creation."
 fi
-pyenv global $PYTHON_VERSION
+enroot start --root --mount $(pwd):/workspace $CONTAINER_NAME <<EOF
+    [ -d ~/.pyenv ] && rm -rf ~/.pyenv
+    echo "Installing pyenv and pyenv-virtualenv..."
+    apt update
+    apt install git -y
+    apt install curl -y
+    apt install -y build-essential gcc make libffi-dev zlib1g-dev libssl-dev libreadline-dev libbz2-dev libsqlite3-dev
+    curl https://pyenv.run | bash
+    export PATH="$HOME/.pyenv/bin:$PATH"
+    eval "$(pyenv init --path)"
+    eval "$(pyenv virtualenv-init -)"
 
-cd $WORKING_DIR
+    if ! pyenv versions | grep -q $PYTHON_VERSION; then
+        echo "Installing Python $PYTHON_VERSION with pyenv..."
+        pyenv install $PYTHON_VERSION
+    fi
+    pyenv global $PYTHON_VERSION
 
-echo "Upgrading pip..."
-pip install --upgrade pip
+    cd $WORKING_DIR
 
-echo "Installing dependencies from requirements.txt..."
-pip install -r $REQUIREMENTS_FILE
+    echo "Upgrading pip..."
+    pip install --upgrade pip
 
-export PYTHONPATH=$(pwd):$PYTHONPATH
+    echo "Installing dependencies from requirements.txt..."
+    pip install -r $REQUIREMENTS_FILE
 
-echo "Running the Python script..."
-python $PYTHON_SCRIPT --kernel_type rbf --encoder_num_iters 1000 --linear_eval_num_iters 200 --encoder_lr 1e-4 --svm_lr 1e-4 --linear_eval_lr 1e-4
+    export PYTHONPATH=$(pwd):$PYTHONPATH
 
-echo "Script completed."
+    echo "Running the Python script..."
+    python $PYTHON_SCRIPT --kernel_type rbf --encoder_num_iters 1000 --linear_eval_num_iters 200 --encoder_lr 1e-4 --svm_lr 1e-4 --linear_eval_lr 1e-4
+
+    echo "Script completed."
+EOF
