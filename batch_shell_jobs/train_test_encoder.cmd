149d3aad00d4c115f33a594cab8216fdde035781
Admir <admir@qyobo.com>
2025-01-12 22:15:50 +0100

    Update batch job

--- batch_shell_jobs/train_test_encoder.cmd
+++ batch_shell_jobs/train_test_encoder.cmd
@@ -11,8 +11,7 @@ echo "Creating and starting the container..."
 enroot remove mmcl_rvcl
 enroot create --name mmcl_rvcl ../nvidia+tensorflow+20.12-tf1-py3.sqsh
 echo "Starting container"
-enroot start --root --mount $(pwd):/workspace mmcl_rvcl << 'EOF'
-
+enroot start --root --mount $(pwd):/workspace mmcl_rvcl
     python --version
 
     echo "Installing dependencies from requirements.txt..."
@@ -24,4 +23,3 @@ enroot start --root --mount $(pwd):/workspace mmcl_rvcl << 'EOF'
     python mmcl/train_test_encoder.py --batch_size 256 --kernel_type rbf --encoder_num_iters 500 --linear_eval_num_iters 200 --encoder_lr 1e-4 --svm_lr 1e-4 --linear_eval_lr 1e-4
 
     echo "Script completed."
-EOF
