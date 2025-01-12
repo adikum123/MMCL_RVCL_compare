3da8a686aaa43b27c59adf321366dbafb12e0642
Admir <admir@qyobo.com>
2025-01-12 22:08:39 +0100

    uPDATE SCRIPT

--- batch_shell_jobs/train_test_encoder.cmd
+++ batch_shell_jobs/train_test_encoder.cmd
@@ -11,17 +11,17 @@ echo "Creating and starting the container..."
 enroot remove mmcl_rvcl
 enroot create --name mmcl_rvcl ../nvidia+tensorflow+20.12-tf1-py3.sqsh
 echo "Starting container"
-enroot start --root --mount $(pwd):/workspace mmcl_rvcl
+enroot start --root --mount $(pwd):/workspace mmcl_rvcl << 'EOF'
 
-python --version
+    python --version
 
-echo "Installing dependencies from requirements.txt..."
-pip install --upgrade pip
-pip install -r requirements.txt
+    echo "Installing dependencies from requirements.txt..."
+    pip install --upgrade pip
+    pip install -r requirements.txt
 
-export PYTHONPATH=$(pwd):$PYTHONPATH
-echo "Running the Python script..."
-python mmcl/train_test_encoder.py --batch_size 256 --kernel_type rbf --encoder_num_iters 500 --linear_eval_num_iters 200 --encoder_lr 1e-4 --svm_lr 1e-4 --linear_eval_lr 1e-4
+    export PYTHONPATH=$(pwd):$PYTHONPATH
+    echo "Running the Python script..."
+    python mmcl/train_test_encoder.py --batch_size 256 --kernel_type rbf --encoder_num_iters 500 --linear_eval_num_iters 200 --encoder_lr 1e-4 --svm_lr 1e-4 --linear_eval_lr 1e-4
 
-echo "Script completed."
-
+    echo "Script completed."
+EOF
