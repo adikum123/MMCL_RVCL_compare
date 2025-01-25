453931e508caf614d9647b9ca6bfb2303767f2ff
Admir <admir@qyobo.com>
2025-01-25 18:43:38 +0100

    Update batch job

--- batch_jobs/train_test_encoder_cifar_clean.cmd
+++ batch_jobs/train_test_encoder_cifar_clean.cmd
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -p lrz-hgx-h100-92x4
 #SBATCH --gres=gpu:2
-#SBATCH --time=1-00:00:00
+#SBATCH --time=5:00:00
 #SBATCH -o outs/cifar_clean_100k.out
 #SBATCH -e outs/cifar_clean_100k.out
 
@@ -26,16 +26,14 @@ enroot start --mount $(pwd):/workspace mmcl_rvcl <<'EOF'
     export PYTHONPATH=$(pwd):$PYTHONPATH
     echo "Training encoder"
     python train_encoder.py \
-        --model_save_name cifar_model_base_poly_deg_5_C_100 \
+        --model_save_name cifar_model_base_rbf_C_100 \
         --model cifar_model \
         --dataset cifar-10 \
         --batch_size 64 \
-        --kernel_type poly \
-        --deegre 5 \
+        --kernel_type rbf \
         --num_iters 200 \
         --lr 1e-4 \
         --step_size 50 \
-        --svm_lr 1e-3 \
         --C 100 \
 
 echo "Testing performance on linear eval"
@@ -44,20 +42,19 @@ echo "Testing performance on linear eval"
         --dataset cifar-10 \
         --use_validation \
         --num_iters 100 \
-        --step_size 50 \
+        --step_size 30 \
         --lr 1e-4 \
-        --load_checkpoint models/mmcl/cifar_model_base_poly_deg_5_C_100.pkl \
+        --load_checkpoint models/mmcl/cifar_model_base_rbf_C_100.pkl \
         --adv_img \
 
     echo "Computing plots for svm margin"
     python compare_svm_margin.py \
         --mmcl_model cifar_model \
-        --mmcl_checkpoint models/mmcl/cifar_model_base_poly_deg_5_C_100.pkl \
+        --mmcl_checkpoint models/mmcl/cifar_model_base_rbf_C_100.pkl \
         --rvcl_model cifar_model \
         --rvcl_checkpoint models/unsupervised/cifar10_base_adv4.pkl \
         --C 100 \
-        --kernel_type poly \
-        --deegre 5 \
+        --kernel_type rbf \
         --class_sample_limit 1000 \
 
 EOF
