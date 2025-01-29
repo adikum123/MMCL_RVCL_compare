ce9e1ec389febf95d95535220c78d3b02c009984
Admir <admir@qyobo.com>
2025-01-29 18:20:17 +0100

    Update batch jobs

--- batch_jobs/train_cifar.cmd
+++ batch_jobs/train_cifar.cmd
@@ -2,8 +2,8 @@
 #SBATCH -p lrz-hgx-h100-92x4
 #SBATCH --gres=gpu:1
 #SBATCH --time=3:00:00
-#SBATCH -o outs/mnist_100k.out
-#SBATCH -e outs/mnist_100k.out
+#SBATCH -o outs/cifar_100k.out
+#SBATCH -e outs/cifar_100k.out
 
 #!/bin/bash
 echo "Creating and starting the container..."
@@ -28,39 +28,27 @@ enroot start --mount $(pwd):/workspace mmcl_rvcl <<'EOF'
     export PYTHONPATH=$(pwd):$PYTHONPATH
     echo "Training encoder"
     python train_encoder.py \
-        --model_save_name mnist_model_base_rbf_C_100 \
-        --model mnist_model_base \
-        --dataset mnist \
+        --model_save_name cifar_model_deep_rbf_C_gamma_auto_100 \
+        --model cifar_model_deep \
+        --dataset cifar-10 \
         --batch_size 32 \
         --kernel_type rbf \
         --num_iters 200 \
-        --use_validation \
         --lr 1e-6 \
+        --use_validation \
         --step_size 50 \
         --C 100 \
 
-    echo "Testing performance on linear eval"
+echo "Testing performance on linear eval"
     python train_linear_eval.py \
         --batch_size 32 \
-        --dataset mnist \
+        --dataset cifar-10 \
         --use_validation \
         --num_iters 100 \
         --step_size 30 \
         --lr 1e-4 \
-        --model mnist_model_base \
-        --load_checkpoint models/mmcl/mnist_model_base_rbf_C_100.pkl \
+        --model cifar_model_deep \
+        --load_checkpoint models/mmcl/cifar_model_deep_rbf_C_gamma_auto_100.pkl \
         --adv_img \
 
-    echo "Computing plots for svm margin"
-    python compare_svm_margin.py \
-        --mmcl_model mnist_model \
-        --mmcl_checkpoint models/mmcl/mnist_model_base_rbf_C_100.pkl \
-        --rvcl_model mnist_model \
-        --rvcl_checkpoint models/unsupervised/mnist_base_adv1.pkl \
-        --dataset mnist \
-        --C 100 \
-        --kernel_type rbf \
-        --class_sample_limit 1000 \
-
-    echo "Script completed."
 EOF
--- batch_jobs/train_mnist.cmd
+++ batch_jobs/train_mnist.cmd
@@ -2,8 +2,8 @@
 #SBATCH -p lrz-hgx-h100-92x4
 #SBATCH --gres=gpu:1
 #SBATCH --time=3:00:00
-#SBATCH -o outs/cifar_100k.out
-#SBATCH -e outs/cifar_100k.out
+#SBATCH -o outs/mnist_100k.out
+#SBATCH -e outs/mnist_100k.out
 
 #!/bin/bash
 echo "Creating and starting the container..."
@@ -28,38 +28,27 @@ enroot start --mount $(pwd):/workspace mmcl_rvcl <<'EOF'
     export PYTHONPATH=$(pwd):$PYTHONPATH
     echo "Training encoder"
     python train_encoder.py \
-        --model_save_name cifar_model_base_rbf_C_100 \
-        --model cifar_model \
-        --dataset cifar-10 \
+        --model_save_name mnist_model_deep_base_rbf_gamma_auto_C_100 \
+        --model mnist_model_deep \
+        --dataset mnist \
         --batch_size 32 \
         --kernel_type rbf \
         --num_iters 200 \
-        --lr 1e-6 \
         --use_validation \
+        --lr 1e-6 \
         --step_size 50 \
         --C 100 \
 
-echo "Testing performance on linear eval"
+    echo "Testing performance on linear eval"
     python train_linear_eval.py \
         --batch_size 32 \
-        --dataset cifar-10 \
+        --dataset mnist \
         --use_validation \
         --num_iters 100 \
         --step_size 30 \
         --lr 1e-4 \
-        --model cifar_model \
-        --load_checkpoint models/mmcl/cifar_model_base_rbf_C_100.pkl \
+        --model mnist_model_deep \
+        --load_checkpoint models/mmcl/mnist_model_deep_base_rbf_gamma_auto_C_100.pkl \
         --adv_img \
-
-    echo "Computing plots for svm margin"
-    python compare_svm_margin.py \
-        --mmcl_model cifar_model \
-        --mmcl_checkpoint models/mmcl/cifar_model_base_rbf_C_100.pkl \
-        --rvcl_model cifar_model \
-        --rvcl_checkpoint models/unsupervised/cifar10_base_adv4.pkl \
-        --dataset cifar-10 \
-        --C 100 \
-        --kernel_type rbf \
-        --class_sample_limit 1000 \
 
 EOF
