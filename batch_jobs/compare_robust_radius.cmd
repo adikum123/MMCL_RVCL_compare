b85d562297d4aa95d1a5a1b973cc029da05f590a
Admir <admir@qyobo.com>
2025-02-02 20:50:54 +0100

    UPdate max steps

--- batch_jobs/compare_robust_radius.cmd
+++ batch_jobs/compare_robust_radius.cmd
@@ -41,6 +41,7 @@ enroot start --mount $(pwd):/workspace mmcl_rvcl <<'EOF'
         --rvcl_model cifar_model_base \
         --rvcl_checkpoint models/unsupervised/cifar10_base_adv4.pkl \
         --dataset cifar-10 \
+        --max_steps 100 \
         --class_sample_limit 20 \
 
 EOF
