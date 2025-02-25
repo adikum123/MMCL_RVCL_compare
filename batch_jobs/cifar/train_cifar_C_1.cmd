d82fc238edcbd0e2c2896b344274fb5e7f9877d2
Admir <admir@qyobo.com>
2025-02-25 23:29:55 +0100

    Small updates

--- batch_jobs/cifar/train_cifar_C_1.cmd
+++ batch_jobs/cifar/train_cifar_C_1.cmd
@@ -49,7 +49,7 @@ enroot start --mount $(pwd):/workspace mmcl_rvcl <<'EOF'
             --step_size 30 \
             --lr 1e-3 \
             --model cnn_4layer_b \
-            --mmcl_checkpoint models/mmcl/poly/cnn_4layer_b_C_1_rbf_auto.pkl \
+            --mmcl_checkpoint models/mmcl/rbf/cnn_4layer_b_C_1_rbf_auto.pkl \
             --adv_img
     else
         echo "Training encoder failed, skipping linear evaluation."
