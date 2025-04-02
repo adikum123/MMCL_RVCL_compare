5c5938fc3b472d6cc70fa876ef54dbff9079ba7b
Admir <admir@qyobo.com>
2025-04-02 14:06:25 +0200

    Small updates

--- batch_jobs/train_linear_eval_regular_cl.cmd
+++ batch_jobs/train_linear_eval_regular_cl.cmd
@@ -37,6 +37,6 @@ enroot start --mount $(pwd):/workspace mmcl_rvcl <<'EOF'
         --model cnn_4layer_b \
         --relu_layer \
         --finetune \
-        --mmcl_checkpoint models/regular_cl/regular_cl_loss_cosine_bs_256_lr_0.001.pkl.pkl
+        --regular_cl_checkpoint models/regular_cl/regular_cl_loss_cosine_bs_256_lr_0.001.pkl.pkl
 
 EOF
