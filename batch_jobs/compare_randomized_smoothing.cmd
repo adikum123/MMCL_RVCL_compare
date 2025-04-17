8ae2111d5b44e0c653f67807a896d5111efef7c0
Admir <admir@qyobo.com>
2025-04-17 21:17:14 +0200

    Small updates

--- batch_jobs/compare_randomized_smoothing.cmd
+++ batch_jobs/compare_randomized_smoothing.cmd
@@ -38,7 +38,7 @@ enroot start --mount $(pwd):/workspace mmcl_rvcl <<'EOF'
 
     echo "Computing plots for robust radius..."
     python -u randomized_smoothing/compare_randomized_smoothing.py \
-        --num_images -1 \
+        --num_images 2000 \
         --finetune
 
 EOF
