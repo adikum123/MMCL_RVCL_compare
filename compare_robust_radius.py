ba02ee94ce492c8e571ceeb364296ba235196828
Admir <admir@qyobo.com>
2025-05-09 11:19:16 +0200

    Small updates

--- compare_robust_radius.py
+++ compare_robust_radius.py
@@ -20,12 +20,12 @@ from beta_crown.modules import Flatten
 from mmcl.utils import Flatten as Flatten1
 from robust_radius import RobustRadius
 
-parser = argparse.ArgumentParser(description='unsupervised binary search')
+parser = argparse.ArgumentParser(description="unsupervised binary search")
 
 ##### arguments for CROWN #####
-parser.add_argument("--norm", type=float, default='inf', help='p norm for epsilon perturbation')
-parser.add_argument("--model", type=str, default="cnn_4layer_b", help='model name (cifar_model, cifar_model_deep, cifar_model_wide, cnn_4layer, cnn_4layer_b, mnist_cnn_4layer)')
-parser.add_argument("--batch_size", type=int, default=256, help='batch size')
+parser.add_argument("--norm", type=float, default="inf", help="p norm for epsilon perturbation")
+parser.add_argument("--model", type=str, default="cnn_4layer_b", help="model name (cifar_model, cifar_model_deep, cifar_model_wide, cnn_4layer, cnn_4layer_b, mnist_cnn_4layer)")
+parser.add_argument("--batch_size", type=int, default=256, help="batch size")
 ##### mmcl and rvcl args #####
 parser.add_argument(
     "--mmcl_model",
@@ -55,25 +55,25 @@ parser.add_argument(
     "--regular_cl_checkpoint", type=str, default="", help="Model checkpoint for rvcl"
 )
 ##### arguments for model #####
-parser.add_argument('--train_type', default='contrastive', type=str, help='contrastive/linear eval/test/supervised')
-parser.add_argument('--dataset', default='cifar-10', type=str, help='cifar-10/mnist')
-parser.add_argument('--name', default='', type=str, help='name of run')
-parser.add_argument('--seed', default=1, type=int, help='random seed')
+parser.add_argument("--train_type", default="contrastive", type=str, help="contrastive/linear eval/test/supervised")
+parser.add_argument("--dataset", default="cifar-10", type=str, help="cifar-10/mnist")
+parser.add_argument("--name", default="", type=str, help="name of run")
+parser.add_argument("--seed", default=1, type=int, help="random seed")
 ##### arguments for data augmentation #####
-parser.add_argument('--color_jitter_strength', default=0.5, type=float, help='0.5 for CIFAR, 1.0 for ImageNet')
-parser.add_argument('--temperature', default=0.5, type=float, help='temperature for pairwise-similarity')
+parser.add_argument("--color_jitter_strength", default=0.5, type=float, help="0.5 for CIFAR, 1.0 for ImageNet")
+parser.add_argument("--temperature", default=0.5, type=float, help="temperature for pairwise-similarity")
 ##### arguments for PGD attack & Adversarial Training #####
-parser.add_argument('--attack_type', type=str, default='linf', help='adversarial l_p')
-parser.add_argument('--target_eps', type=float, default=16.0/255, help='maximum perturbation of adversaries (8/255 0.0314 for cifar-10)')
-parser.add_argument('--alpha', type=float, default=0.001, help='movement multiplier per iteration when generating adversarial examples (2/255=0.00784)')
-parser.add_argument('--k', type=int, default=150, help='maximum iteration when generating adversarial examples')
-parser.add_argument('--random_start', type=bool, default=True, help='True for PGD')
-parser.add_argument('--loss_type', type=str, default='mse', help='loss type for Rep: mse/sim/l1/cos')
+parser.add_argument("--attack_type", type=str, default="linf", help="adversarial l_p")
+parser.add_argument("--target_eps", type=float, default=16.0/255, help="maximum perturbation of adversaries (8/255 0.0314 for cifar-10)")
+parser.add_argument("--alpha", type=float, default=0.001, help="movement multiplier per iteration when generating adversarial examples (2/255=0.00784)")
+parser.add_argument("--k", type=int, default=150, help="maximum iteration when generating adversarial examples")
+parser.add_argument("--random_start", type=bool, default=True, help="True for PGD")
+parser.add_argument("--loss_type", type=str, default="mse", help="loss type for Rep: mse/sim/l1/cos")
 ##### arguments for binary_search #####
-parser.add_argument('--mini_batch', type=int, default=10, help='mini batch for PGD')
-parser.add_argument('--max_steps', type=int, default=200, help='max steps for search')
-parser.add_argument("--negatives_per_class", type=int, default=5, help='number of negative items chosen per class')
-parser.add_argument("--positives_per_class", type=int, default=5, help='number of negative items chosen per class')
+parser.add_argument("--mini_batch", type=int, default=10, help="mini batch for PGD")
+parser.add_argument("--max_steps", type=int, default=200, help="max steps for search")
+parser.add_argument("--negatives_per_class", type=int, default=5, help="number of negative items chosen per class")
+parser.add_argument("--positives_per_class", type=int, default=5, help="number of negative items chosen per class")
 parser.add_argument("--num_retries", type=int, default=3, help="number of retries to minimize effect of randomness")
 args = parser.parse_args()
 
@@ -83,7 +83,7 @@ torch.cuda.manual_seed_all(args.seed)
 random.seed(args.seed)
 np.random.seed(args.seed)
 _, _, _, _, testloader, testdst = data_loader.get_train_val_test_dataset(args=args)
-device = torch.device('cpu')
+device = torch.device("cpu")
 
 
 # load models then create verifiers
@@ -123,7 +123,7 @@ def compute_radius(verifier, ori_image, target_image):
 # for each sample in class we compute the average radius
 average_robust_radius = defaultdict(list)
 for class_index, (class_name, class_ori_images) in enumerate(original_images.items()):
-    print(f'Processing class: {class_name}, already processed: {class_index+1}/{len(class_names)} classes')
+    print(f"Processing class: {class_name}, already processed: {class_index+1}/{len(class_names)} classes")
     for retry in range(args.num_retries):
         print(f"Processing retry: {retry+1}")
         target_images = [image for k, v in per_class_sampler.items() for image in random.sample(v, args.negatives_per_class)]
@@ -153,9 +153,9 @@ for class_index, (class_name, class_ori_images) in enumerate(original_images.ite
                 )
             print(f"Calculated lists of values:\nmmcl:{mmcl_robust_radius}\nrvcl:{rvcl_robust_radius}\nregular cl: {regular_cl_radius}")
             average_robust_radius[f"{image_index}|{retry}"].append({
-                'mmcl': sum(mmcl_robust_radius) / len(mmcl_robust_radius),
-                'rvcl': sum(rvcl_robust_radius) / len(rvcl_robust_radius),
-                'regular_cl': sum(regular_cl_radius) / len(regular_cl_radius)
+                "mmcl": sum(mmcl_robust_radius) / len(mmcl_robust_radius),
+                "rvcl": sum(rvcl_robust_radius) / len(rvcl_robust_radius),
+                "regular_cl": sum(regular_cl_radius) / len(regular_cl_radius)
             })
 print(f"Average robust radius dict:\n{json.dumps(average_robust_radius, indent=4)}")
 per_image_values = defaultdict(list)
@@ -183,7 +183,7 @@ def get_model_name_from_ckpt(ckpt):
 save_dir = "radius_results"
 # Ensure the directory exists
 os.makedirs(save_dir, exist_ok=True)
-output_file_name = "-".join([x.replace(" ", "_") for x in [args.mmcl_model, args.rvcl_model, args.regular_cl_model]])
+file_name = "-".join([x.replace(" ", "_") for x in [args.mmcl_model, args.rvcl_model, args.regular_cl_model]])
 with open(os.path.join(save_dir, f"{file_name}.json"), "w") as f:
     json.dump(per_model_mean_std, f, indent=4)
 
